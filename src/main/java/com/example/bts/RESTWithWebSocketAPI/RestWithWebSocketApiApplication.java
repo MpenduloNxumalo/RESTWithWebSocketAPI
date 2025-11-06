f27c77436cc2ea6759ed1be4c62a2a39196342db
MpenduloNxumalo <MpenduloN@discovery.co.za>
2025-11-06 14:51:32 +0200

    feat(code-style): apply google java format
    Apply google java code style to project using maven formatter plugin
    
    - Add eclipse google style xml file.
    - Apply code formatting to main and test java files.
    - Improve code readability.

--- src/main/java/com/example/bts/RESTWithWebSocketAPI/RestWithWebSocketApiApplication.java
+++ src/main/java/com/example/bts/RESTWithWebSocketAPI/RestWithWebSocketApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestWithWebSocketApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestWithWebSocketApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RestWithWebSocketApiApplication.class, args);
+  }
 
 }
