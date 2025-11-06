4d1afa2254ad0128d65b4edab46dc1924c148bf2
MpenduloNxumalo <MpenduloN@discovery.co.za>
2025-11-06 14:19:00 +0200

    feat(initial): initial project setup
    - Create initial project structure.
    - Add necessary dependencies for REST and WebSocket API.
    - Create basic application files.

---
+++ src/main/java/com/example/bts/RESTWithWebSocketAPI/RestWithWebSocketApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bts.RESTWithWebSocketAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithWebSocketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithWebSocketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bts/RESTWithWebSocketAPI/RestWithWebSocketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bts.RESTWithWebSocketAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithWebSocketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
